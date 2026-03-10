c1f9692d04ad78508e4628d17ba766e34bf4b1f4
serhiyben <serhiyben2@gmail.com>
2026-03-10 23:41:48 +0200

    lab1

---
+++ app/schemas/book.py
@@ -0,0 +1,21 @@
+from pydantic import BaseModel, Field
+from uuid import UUID, uuid4
+from typing import Optional
+from enum import Enum
+
+class BookStatus(str, Enum):
+    AVAILABLE = "наявна"
+    BORROWED = "видана"
+
+class BookBase(BaseModel):
+    title: str = Field(..., min_length=1, json_schema_extra={"example": "All Quiet on the Western Front"})
+    author: str = Field(..., min_length=1, json_schema_extra={"example": "Erich Maria Remarque"})
+    description: Optional[str] = Field(None, json_schema_extra={"example": "depicting the extreme..."})
+    status: BookStatus = BookStatus.AVAILABLE
+    year: int = Field(..., gt=0, le=2026)
+
+class BookCreate(BookBase):
+    pass
+
+class Book(BookBase):
+    id: UUID = Field(default_factory=uuid4)
